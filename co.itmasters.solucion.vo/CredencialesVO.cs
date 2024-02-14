118ed469ce2a2738632ba3dee3f495548efa5371
Diego ramirez <diegoalexander.ramirezflorez@gmail.com>
2024-02-14 11:17:04 -0500

    frist

---
+++ co.itmasters.solucion.vo/CredencialesVO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace co.itmasters.solucion.vo
+{
+     [Serializable]
+     class CredencialesVO
+    {
+        public string id { get; set; }
+
+    }
+}
