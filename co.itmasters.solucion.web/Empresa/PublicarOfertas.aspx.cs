6ca77181869f42afb83986b462c9cf93034f2e0c
Diego ramirez <diegoalexander.ramirezflorez@gmail.com>
2024-02-28 17:16:47 -0500

    rangos de fchas con relacion a planadquirido

--- co.itmasters.solucion.web/Empresa/PublicarOfertas.aspx.cs
+++ co.itmasters.solucion.web/Empresa/PublicarOfertas.aspx.cs
@@ -14,6 +14,8 @@ using System.IO;
 using co.itmasters.solucion.web.EmpresaService;
 using static System.Net.Mime.MediaTypeNames;
 using Microsoft.Win32;
+using co.itmasters.solucion.vo.constantes;
+using CrystalDecisions.Data;
 
 namespace co.itmasters.solucion.web.Empresa
 {
@@ -22,6 +24,7 @@ namespace co.itmasters.solucion.web.Empresa
         private OfertaServiceClient _OfertaService;
         private EmpresaServiceClient _ActoresService;
         private UserVO user;
+        
         private CargaCombos _carga = new CargaCombos();
         private List<Control> tags = new List<Control>();
         private List<Label> tagsNames = new List<Label>();
@@ -29,12 +32,14 @@ namespace co.itmasters.solucion.web.Empresa
 
 
         protected void Page_Load(object sender, EventArgs e)
-        {  
-           
+        {
+
             if (!Page.IsPostBack)
 
             {
+                RangeDate();
                 user = ((UserVO)Session["UsuarioAutenticado"]);
+                
                 if (Request.QueryString["idOferta"] == null)
                 {
                     ViewState["IdOferta"] = "0";
@@ -54,6 +59,36 @@ namespace co.itmasters.solucion.web.Empresa
             }
             this.FillList();
 
+
+        }
+        protected void RangeDate()
+        {
+            user = ((UserVO)Session["UsuarioAutenticado"]);
+            OfertaVO datosConsulta = new OfertaVO();
+            datosConsulta.typeModify = TipoConsulta.GET;
+            datosConsulta.idUsuario = user.IdUsuario;
+            datosConsulta.estado = EstadoPago.ESTADO_CONCILIADO;
+
+            _OfertaService = new OfertaServiceClient();
+            List<OfertaVO> resultado = _OfertaService.TraePlanesAdquiridosEmpresa(datosConsulta).ToList();
+            _OfertaService.Close();
+
+
+            OfertaVO plan = resultado.Find(e => e.fechaFinaliza > DateTime.Now && e.fechaInicia < DateTime.Now);
+
+            lblDiasOferta.Text = plan.diasPublicacionOferta.ToString();
+
+            rvFechaPublicacion.MinimumValue = String.Format("{0:yyyy-MM-dd}", DateTime.Now);
+            txtFechaPublicacion.Attributes["min"] = String.Format("{0:yyyy-MM-dd}", DateTime.Now);
+            rvFechaPublicacion.MaximumValue = String.Format("{0:yyyy-MM-dd}", plan.fechaFinaliza);
+            txtFechaPublicacion.Attributes["max"] = String.Format("{0:yyyy-MM-dd}", plan.fechaFinaliza);
+
+
+            rvFechaVencimiento.MinimumValue = String.Format("{0:yyyy-MM-dd}", DateTime.Now);
+            txtFechaVencimiento.Attributes["min"] = String.Format("{0:yyyy-MM-dd}", DateTime.Now);
+            rvFechaVencimiento.MaximumValue = String.Format("{0:yyyy-MM-dd}", DateTime.Now);
+            txtFechaVencimiento.Attributes["max"] = String.Format("{0:yyyy-MM-dd}", DateTime.Now);
+            
 
         }
         protected void cargarCombos()
@@ -471,6 +506,15 @@ namespace co.itmasters.solucion.web.Empresa
         protected void btnCloseModal_Click(object sender, EventArgs e)
         {
             ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Prueba", $"CloseModal('{modalOfertaCreada.ClientID}', '{openModal.ClientID}')", true);
+        }
+
+        protected void txtFechaPublicacion_TextChanged(object sender, EventArgs e)
+        {
+            DateTime fechaPublicacion = Convert.ToDateTime(txtFechaPublicacion.Text);
+            Int32 diasOferta = Convert.ToInt32(lblDiasOferta.Text);
+            DateTime fechaMaxima = fechaPublicacion + TimeSpan.FromDays(diasOferta);
+            rvFechaVencimiento.MaximumValue = String.Format("{0:yyyy-MM-dd}", fechaMaxima);
+            txtFechaVencimiento.Attributes["max"] = String.Format("{0:yyyy-MM-dd}", fechaMaxima);
         }
     }
 }
